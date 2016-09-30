7a4f6dde78aa85a39315651a9ec23c9c512798a2
Leonard Ng'eno <dranoel.bravo@gmail.com>
2016-09-30 09:36:03 -0400

    move fragment navigation actions to options menu

--- app/src/main/java/org/chpir/android/roster/ParticipantEditorActivity.java
+++ app/src/main/java/org/chpir/android/roster/ParticipantEditorActivity.java
@@ -7,12 +7,9 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
-import android.widget.Button;
 
 import org.chpir.android.roster.Models.Participant;
 import org.chpir.android.roster.Models.Question;
@@ -29,8 +26,6 @@ public class ParticipantEditorActivity extends AppCompatActivity {
     private NavigationView navigationView;
     private int currentMenuItem = 0;
     private Participant mParticipant;
-    private Button mPrevious;
-    private Button mNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,21 +48,6 @@ public class ParticipantEditorActivity extends AppCompatActivity {
         navigationView = (NavigationView) findViewById(R.id.roster_drawer_view);
         setNavigationViewMenu();
         setNavigationViewListener(navigationView);
-
-        mPrevious = (Button) findViewById(R.id.previous_question);
-        mNext = (Button) findViewById(R.id.next_question);
-        mPrevious.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateFragment(currentMenuItem - 1);
-            }
-        });
-        mNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                updateFragment(currentMenuItem + 1);
-            }
-        });
     }
 
     @Override
@@ -99,45 +79,6 @@ public class ParticipantEditorActivity extends AppCompatActivity {
                 });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.participant_editor_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                mDrawer.openDrawer(GravityCompat.START);
-                return true;
-            case R.id.save_new_participant:
-                saveParticipant();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-    private void saveParticipant() {
-        Log.i(TAG, "Called By: " + getCallingActivity());
-        Class callingClass = RosterActivity.class;
-        if (getCallingActivity() != null) {
-            callingClass = getCallingActivity().getClass();
-        }
-        Intent intent = new Intent(ParticipantEditorActivity.this, callingClass);
-        intent.putExtra("Participant", Parcels.wrap(mParticipant));
-        setResult(100, intent);
-        finish();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        updateFragment(currentMenuItem);
-    }
-
     private void updateFragment(int position) {
         currentMenuItem = position;
         RosterFragment fragment = RosterFragmentGenerator.createQuestionFragment(
@@ -146,7 +87,7 @@ public class ParticipantEditorActivity extends AppCompatActivity {
         bundle.putParcelable("Question", Parcels.wrap(mParticipant.getQuestions().get(position)));
         fragment.setArguments(bundle);
         switchOutFragment(fragment);
-        hideNavigationButtons();
+        invalidateOptionsMenu();
         checkMenuItem(navigationView.getMenu().getItem(currentMenuItem));
     }
 
@@ -156,19 +97,6 @@ public class ParticipantEditorActivity extends AppCompatActivity {
             manager.beginTransaction().add(R.id.roster_item_container, fragment).commit();
         } else {
             manager.beginTransaction().replace(R.id.roster_item_container, fragment).commit();
-        }
-    }
-
-    private void hideNavigationButtons() {
-        Log.i(TAG, "currentMenuItem: " + currentMenuItem);
-        Log.i(TAG, "num of items: " + mParticipant.getQuestions().size());
-        if (currentMenuItem == 0) {
-            if (mPrevious != null) mPrevious.setVisibility(View.INVISIBLE);
-        } else if (currentMenuItem == mParticipant.getQuestions().size() - 1) {
-            if (mNext != null) mNext.setVisibility(View.INVISIBLE);
-        } else {
-            if (mNext != null) mNext.setVisibility(View.VISIBLE);
-            if (mPrevious != null) mPrevious.setVisibility(View.VISIBLE);
         }
     }
 
@@ -185,6 +113,60 @@ public class ParticipantEditorActivity extends AppCompatActivity {
                 item.setChecked(false);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.participant_editor_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        if (currentMenuItem == 0) {
+            menu.findItem(R.id.menu_item_previous).setVisible(false);
+        } else if (currentMenuItem == mParticipant.getQuestions().size() - 1) {
+            menu.findItem(R.id.menu_item_next).setVisible(false);
+        }
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.save_new_participant:
+                saveParticipant();
+                return true;
+            case R.id.menu_item_next:
+                updateFragment(currentMenuItem + 1);
+                return true;
+            case R.id.menu_item_previous:
+                updateFragment(currentMenuItem - 1);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveParticipant() {
+        Class callingClass = RosterActivity.class;
+        if (getCallingActivity() != null) {
+            callingClass = getCallingActivity().getClass();
+        }
+        Intent intent = new Intent(ParticipantEditorActivity.this, callingClass);
+        intent.putExtra("Participant", Parcels.wrap(mParticipant));
+        setResult(100, intent);
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateFragment(currentMenuItem);
     }
 
 }
